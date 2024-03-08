106cf8dfe99ac9e5e6edfc6756be9e84cbddf7d3
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 14:10:40 -0500

    Adding an example entity WeatherForecast.cs that inherits from EntityBase in Domain

---
+++ source/Domain/Entities/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace Source.Domain.Entities;
+
+public class WeatherForecast : EntityBase
+{
+    public DateOnly Date { get; set; }
+
+    public string City { get; set; }
+    public int TempCelsius { get; set; }
+
+    public int TempFahrenheit => 32 + (int)(TempCelsius / 0.5556);
+
+    public string? Summary { get; set; }
+}
