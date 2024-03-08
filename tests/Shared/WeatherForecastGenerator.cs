299559e02821074d04eea06ad88a545161236d13
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 14:44:31 -0500

    Taking the array of summaries from the WeatherForecastController and moving it to the WeatherForecastGenerator.cs to use with the Summary field in the WeatherForecast entity

--- tests/Shared/WeatherForecastGenerator.cs
+++ tests/Shared/WeatherForecastGenerator.cs
@@ -11,6 +11,10 @@ public static class WeatherForecastGenerator
     private static uint _id = 1;
     public static List<WeatherForecast>? WeatherForecasts { get; set; }
     private static Faker? _f;
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
     
     /// <summary>
     /// Generates a specified number of <see cref="WeatherForecast"/> using a <see cref="Faker"/> instance
@@ -28,7 +32,7 @@ public static class WeatherForecastGenerator
                 Id = _id++,
                 Date = _f.Date.FutureDateOnly(0, DateOnly.FromDateTime(DateTime.Now)),
                 TempCelsius = _f.Random.Number(0, 100),
-                Summary = _f.Lorem.Sentence(),
+                Summary = _f.Random.ArrayElement(Summaries),
                 City = _f.Address.City(),
                 CreatedBy = "system",
                 CreatedAt = _f.Date.Past(1, DateTime.Now)
