645dd60865c0f8a6eab56f796acc3debda95680f
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 10:01:53 -0500

    Adding a Shared library in the Tests folder that will be shared amongst all the Unit and Integration test projects

---
+++ tests/Shared/CustomXunitTraits/ArchitectureTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Tests.Shared.CustomXunitTraits;
+
+/// <summary>
+/// Apply this attribute to your xUnit test method to specify it is an Architecture test
+/// </summary>
+/// <remarks>https://www.brendanconnolly.net/organizing-tests-with-xunit-traits/</remarks>
+[TraitDiscoverer(nameof(ArchitectureTestDiscoverer), nameof(Shared))] 
+[AttributeUsage(AttributeTargets.Class)]
+public class ArchitectureTestAttribute : Attribute, ITraitAttribute;
+
+public class ArchitectureTestDiscoverer : ITraitDiscoverer
+{
+    public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+    {
+        yield return new KeyValuePair<string, string>("Category", "ArchitectureTest");
+    }
+}
