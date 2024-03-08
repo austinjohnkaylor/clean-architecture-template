7b6353d19c78cdb14019aa411b761293bd33818a
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 16:21:04 -0500

    Adding an xunit trait attribute for ExceptionTestCases

---
+++ tests/Shared/CustomXunitTraits/ExceptionTestCase.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests.Shared.CustomXunitTraits;
+
+/// <summary>
+/// Apply this attribute to your xUnit test method to specify it is an Exception test case
+/// </summary>
+/// <remarks>https://www.brendanconnolly.net/organizing-tests-with-xunit-traits/</remarks>
+[TraitDiscoverer("Tests.Shared.CustomXunitTraits.ExceptionTestCaseDiscoverer", nameof(Shared))]
+[AttributeUsage(AttributeTargets.Class)]
+public class ExceptionTestCaseAttribute : Attribute, ITraitAttribute
+{
+    public ExceptionTestCaseAttribute()
+    {
+        
+    }
+}
+
+public class ExceptionTestCaseDiscoverer : ITraitDiscoverer
+{
+    public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+    {
+        yield return new KeyValuePair<string, string>("Category", "Exception Test Case");
+    }
+}
