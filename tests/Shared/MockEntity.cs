03f25c900e5581be7e28c59447452c6f26ed6972
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 13:46:26 -0500

    Creating a mock entity inheriting the EntityBase class with a Bogus generator called MockEntityGenerator.cs

---
+++ tests/Shared/MockEntity.cs
@@ -0,0 +1,14 @@
+﻿using Source.Domain;
+
+namespace Tests.Shared;
+
+/// <summary>
+/// A mock entity for used for testing purposes.
+/// </summary>
+public class MockEntity : EntityBase
+{
+    public string FirstName { get; set; }
+    public char MiddleInitial { get; set; }
+    public string LastName { get; set; }
+    public string Age { get; set; }
+}
