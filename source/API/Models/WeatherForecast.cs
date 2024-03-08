79942a45d2f7bc6f70c0237464c5b7922c331f1f
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 14:10:09 -0500

    Adding City to the WeatherForecast.cs API model

--- source/API/Models/WeatherForecast.cs
+++ source/API/Models/WeatherForecast.cs
@@ -4,6 +4,7 @@ public class WeatherForecast
 {
     public DateOnly Date { get; set; }
 
+    public string City { get; set; }
     public int TemperatureC { get; set; }
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
