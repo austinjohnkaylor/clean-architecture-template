e6065540f6a116275b911c781ba87624d2fb288e
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 08:36:57 -0500

    Adding a DependencyInjection.cs class to add all the services required for the Application layer

---
+++ source/Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Source.Application;
+
+/// <summary>
+/// A class for injecting services into microsoft's service collection 
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Registers all dependencies for the Application layer.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static void AddApplicationLayer(this IServiceCollection services)
+    {
+        services.AddMediator();
+    }
+}
