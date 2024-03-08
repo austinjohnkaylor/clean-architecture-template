1e114d3e3550478c4e923c4015b53da1dd574800
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 10:46:21 -0500

    simple refactoring in API's program.cs and registering the Application and Infrastructure layers

--- source/API/Program.cs
+++ source/API/Program.cs
@@ -1,13 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using Source.Application;
+using Source.Infrastructure;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string? databaseConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
+builder.Services.AddApplicationLayer();
+builder.Services.AddInfrastructureLayer(databaseConnectionString);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
