ef08db1deedad2e9a254e8220c0fa1d73dafb8a0
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 09:33:38 -0500

    Adding an interface for a unit of work to the Application project

---
+++ source/Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace Source.Application.Interfaces;
+
+/// <summary>
+/// An interface for the Unit of Work pattern
+/// </summary>
+/// <remarks>https://code-maze.com/csharp-unit-of-work-pattern/</remarks>
+public interface IUnitOfWork
+{
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+}
