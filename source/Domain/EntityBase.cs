b2b6152fad9e25e6ab6a5cf8bb2744f40be56e10
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 08:29:28 -0500

    Adding a Domain project with a base entity class

---
+++ source/Domain/EntityBase.cs
@@ -0,0 +1,40 @@
+﻿namespace Source.Domain;
+
+/// <summary>
+/// A base entity that all other entities should inherit from
+/// </summary>
+public class EntityBase
+{
+    /// <summary>
+    /// The unique identifier for this entity.
+    /// </summary>
+    public uint Id { get; set; }
+    /// <summary>
+    /// The date and time this entity was created.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+    /// <summary>
+    /// The date and time this entity was last updated.
+    /// </summary>
+    public DateTime UpdatedAt { get; set; }
+    /// <summary>
+    /// The date and time this entity was deleted.
+    /// </summary>
+    public DateTime? DeletedAt { get; set; }
+    /// <summary>
+    /// When true, this entity is soft deleted.
+    /// </summary>
+    public bool IsDeleted { get; set; }
+    /// <summary>
+    /// Who created this entity.
+    /// </summary>
+    public string CreatedBy { get; set; }
+    /// <summary>
+    /// Who last updated this entity.
+    /// </summary>
+    public string? UpdatedBy { get; set; }
+    /// <summary>
+    /// Who deleted this entity.
+    /// </summary>
+    public string? DeletedBy { get; set; }
+}
