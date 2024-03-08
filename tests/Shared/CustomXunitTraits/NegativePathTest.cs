2b7da425ca7d9e8d3a22102513ae08c1141c506b
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 16:02:43 -0500

    Adding a custom xunit trait for negative test cases

---
+++ tests/Shared/CustomXunitTraits/NegativePathTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests.Shared.CustomXunitTraits;
+
+/// <summary>
+/// Apply this attribute to an xUnit test method to specify it is a Negative Path (a.k.a Happy Path) test
+/// </summary>
+/// <remarks>https://www.brendanconnolly.net/organizing-tests-with-xunit-traits/</remarks>
+[TraitDiscoverer("Tests.Shared.CustomXunitTraits.NegativePathTestDiscoverer", nameof(Shared))]
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class NegativePathTestAttribute : Attribute, ITraitAttribute
+{
+    public NegativePathTestAttribute()
+    {
+        
+    }
+}
+
+public class NegativePathTestDiscoverer : ITraitDiscoverer
+{
+    public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+    {
+        yield return new KeyValuePair<string, string>("Category", "Negative Path Test");
+    }
+}
