726ee1647a9f1e2fc1b71a4e806198373453ab4c
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 16:01:26 -0500

    Renaming HappyPathTest.cs to PositivePathTest.cs because I'm also going to have a NegativePathTest attribute

---
+++ tests/Shared/CustomXunitTraits/PositivePathTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests.Shared.CustomXunitTraits;
+
+/// <summary>
+/// Apply this attribute to an xUnit test method to specify it is a Positive Path (a.k.a Happy Path) test
+/// </summary>
+/// <remarks>https://www.brendanconnolly.net/organizing-tests-with-xunit-traits/</remarks>
+[TraitDiscoverer("Tests.Shared.CustomXunitTraits.PositivePathTestDiscoverer", nameof(Shared))]
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class PositivePathTestAttribute : Attribute, ITraitAttribute
+{
+    public PositivePathTestAttribute()
+    {
+        
+    }
+}
+
+public class PositivePathTestDiscoverer : ITraitDiscoverer
+{
+    public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+    {
+        yield return new KeyValuePair<string, string>("Category", "Positive Path Test");
+    }
+}
