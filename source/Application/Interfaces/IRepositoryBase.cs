f53321ea2683628604cb990dece8b4e68b84ca5f
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-08 09:33:15 -0500

    Adding an interface for a Repository base class to the Application project

---
+++ source/Application/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using Source.Domain;
+
+namespace Source.Application.Interfaces;
+
+/// <summary>
+/// Represents a base repository for all other repositories.
+/// </summary>
+/// <typeparam name="T">The domain entity of type T</typeparam>
+public interface IRepositoryBase<T> where T : EntityBase
+{
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+
+    Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+    Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+    
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    Task UpdateRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+    
+    Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+    Task DeleteRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+}
