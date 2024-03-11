c2dfc9dd0783d57fab6d35014989ae0331ccd89a
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-11 11:09:54 -0400

    Adding a WeatherForecastService.cs and an interface for it to interact with WeatherForecasts

---
+++ source/Application/Services/WeatherForecastService.cs
@@ -0,0 +1,40 @@
+﻿using Source.Application.Interfaces;
+using Source.Domain.Entities;
+
+namespace Source.Application.Services;
+
+public class WeatherForecastService : IWeatherForecastService
+{
+    public WeatherForecastService(IUnitOfWork unitOfWork)
+    {
+        ArgumentNullException.ThrowIfNull(unitOfWork);
+        _unitOfWork = unitOfWork;
+    }
+    
+    public Task<IEnumerable<WeatherForecast>> GetForecastsAsync()
+    {
+        return _unitOfWork.WeatherForecasts.GetAllAsync();
+    }
+    
+    public Task<WeatherForecast> GetForecastAsync(uint id)
+    {
+        return _unitOfWork.WeatherForecasts.GetByIdAsync(id);
+    }
+    
+    public Task<WeatherForecast> CreateForecastAsync(WeatherForecast forecast)
+    {
+        return _unitOfWork.WeatherForecasts.AddAsync(forecast);
+    }
+    
+    public Task<WeatherForecast> UpdateForecastAsync(WeatherForecast forecast)
+    {
+        return _unitOfWork.WeatherForecasts.UpdateAsync(forecast) as Task<WeatherForecast>;
+    }
+    
+    public Task DeleteForecastAsync(uint id)
+    {
+        return _unitOfWork.WeatherForecasts.DeleteAsync(id);
+    }
+    
+    private readonly IUnitOfWork _unitOfWork;
+}
