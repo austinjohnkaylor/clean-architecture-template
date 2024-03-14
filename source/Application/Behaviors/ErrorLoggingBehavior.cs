b729d5d5ec30fe509035175d47fe2d432a30dc76
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-13 21:49:04 -0400

    Adding a behavior for ErrorLogging to the mediator pipeline

---
+++ source/Application/Behaviors/ErrorLoggingBehavior.cs
@@ -0,0 +1,19 @@
+﻿using Mediator;
+using Microsoft.Extensions.Logging;
+
+namespace Source.Application.Behaviors;
+
+public sealed class ErrorLoggingBehaviour<TMessage, TResponse>(
+    ILogger<ErrorLoggingBehaviour<TMessage, TResponse>> logger) : MessageExceptionHandler<TMessage, TResponse>
+    where TMessage : notnull, IMessage
+{
+    protected override ValueTask<ExceptionHandlingResult<TResponse>> Handle(
+        TMessage message,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
+    {
+        logger.LogError(exception, "Error handling message of type {messageType}", message.GetType().Name);
+        return NotHandled;
+    }
+}
