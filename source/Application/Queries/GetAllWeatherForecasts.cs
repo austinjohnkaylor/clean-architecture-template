b622e83f92e4eb90f86409a521e24600e4a75941
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-13 20:50:44 -0400

    Writing a query and query handler for getting all weather forecasts

---
+++ source/Application/Queries/GetAllWeatherForecasts.cs
@@ -0,0 +1,16 @@
+﻿using Mediator;
+using Source.Application.Interfaces;
+using Source.Domain.Entities;
+
+namespace Source.Application.Queries;
+
+public class GetAllWeatherForecastsQuery : IRequest<IEnumerable<WeatherForecast>>;
+
+public class GetAllWeatherForecastsQueryHandler(IUnitOfWork unitOfWork)
+    : IRequestHandler<GetAllWeatherForecastsQuery, IEnumerable<WeatherForecast>>
+{
+    public async ValueTask<IEnumerable<WeatherForecast>> Handle(GetAllWeatherForecastsQuery request, CancellationToken cancellationToken)
+    {
+        return await unitOfWork.WeatherForecasts.GetAllAsync(cancellationToken);
+    }
+}
