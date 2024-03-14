7bdc0cdcd97ff3fa93a7bd45e513a2999333ddf5
Austin Kaylor <AustinJKaylor@eaton.com>
2024-03-13 21:02:35 -0400

    Creating a new command for creating a weather forecast

---
+++ source/Application/Commands/CreateWeatherForecast.cs
@@ -0,0 +1,22 @@
+﻿using Mediator;
+using Source.Application.Interfaces;
+using Source.Domain.Entities;
+
+namespace Source.Application.Commands;
+
+public class CreateWeatherForecastCommand(WeatherForecast weatherForecast) : IRequest<WeatherForecast>
+{
+    public WeatherForecast WeatherForecast { get; set; } = weatherForecast;
+}
+
+public class CreateWeatherForecastCommandHandler(IUnitOfWork unitOfWork)
+    : IRequestHandler<CreateWeatherForecastCommand, WeatherForecast>
+{
+    public async ValueTask<WeatherForecast> Handle(CreateWeatherForecastCommand request, CancellationToken cancellationToken)
+    {
+        WeatherForecast createdWeatherForecast = await unitOfWork.WeatherForecasts.AddAsync(request.WeatherForecast, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return createdWeatherForecast;
+    }
+}
+
